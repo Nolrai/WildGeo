a8569936f79461f601192e6c49762615c2760d2c
Chris A. Upshaw <chrisaupshaw@gmail.com>
2022-06-07 04:14:12 -0700

    Working now.

--- lakefile.lean
+++ lakefile.lean
@@ -3,5 +3,8 @@ open Lake DSL
 
 package WildGeo {
   -- add configuration options here
-  
+    dependencies := #[{
+    name := `mathlib
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "master"
+  }]
 }
